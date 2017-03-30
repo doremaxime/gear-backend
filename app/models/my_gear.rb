d4cc55f81b37e82ec90c10a2bec0b51289542833
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-03-30 15:07:31 -0400

    Set character limits to my_gears and projects
    All attributes have a minimum requirement of 1 character.
    my_gears: name max is 10, quantity max is 14.
    my_gears: comments max is 11.
    
    This has been set to reflect the front-end.
    Limit can be removed after March 31st 2017.

--- app/models/my_gear.rb
+++ app/models/my_gear.rb
@@ -3,7 +3,8 @@
 # :nodoc:
 class MyGear < ApplicationRecord
   belongs_to :user
-
+  validates_length_of :name, minimum: 1, maximum: 10, allow_blank: false
+  validates_length_of :quantity, minimum: 1, maximum: 14, allow_blank: false
   validates :name, presence: true
   validates :quantity, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,6 @@
 # :nodoc:
 class Project < ApplicationRecord
   belongs_to :user
-
+  validates_length_of :comment, minimum: 1, maximum: 11, allow_blank: false
   validates :comment, presence: true
 end
